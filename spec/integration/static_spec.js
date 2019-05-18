424d86c72466a1f1b09539757bd11ca4cc560972
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-05-18 14:36:25 -0700

    test passing

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -7,7 +7,6 @@ describe('routes: static', () => {
   describe('GET /', () => {
     it('shold return a status code 200 and say "welcome to the Wall" in the body', (done) => {
       request.get(base, (err, res, body) => {
-        console.log('{TEST 1} BODY: ', body);
         expect(res.statusCode).toBe(200);
         expect(body).toContain('Welcome to the Wall');
         expect(err).toBeNull();
