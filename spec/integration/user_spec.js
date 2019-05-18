6dc3bb6bbc4ed251a4975a2d63e749e98c426fcb
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-05-18 16:09:25 -0700

    user signup view created

---
+++ spec/integration/user_spec.js
@@ -0,0 +1,33 @@
+const request = require('request');
+const server = require('../../src/server');
+const base = 'http://localhost:3000/users/';
+const User = require('../../src/db/models').User;
+const sequelize = require('../../src/db/models/index').sequelize;
+
+describe('routes: users', () => {
+  beforeEach((done) => {
+    sequelize.sync({force: true})
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      console.log(err);
+      done();
+    });
+  });
+
+  describe('GET /users/signUp', () => {
+    it('should render a view with a sign up form', (done) => {
+      request.get(`${base}sign_up`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain('Sign Up');
+        done();
+      });
+    });
+
+    // END USER SIGN UP 
+  });
+
+  
+  // END OF USER INTEGRATION TESTS
+})
