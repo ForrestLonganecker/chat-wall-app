f540dc0b92fe34f8d9e4e6160cd2b99f861a90ef
Forrest Longanecker <ForrLong.Tech@gmail.com>
2019-05-19 11:15:56 -0700

    validation working

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController.js');
+const validation = require('./validation');
 
 router.get('/posts', postController.index);
 
-router.post('/posts/create', postController.create);
+router.post('/posts/create', validation.validatePost, postController.create);
 
 module.exports = router;
--- src/routes/validation.js
+++ src/routes/validation.js
@@ -1,6 +1,6 @@
 module.exports = {
   validateUsers(req, res, next){
-    if(req.method === 'post'){
+    if(req.method == 'post'){
       req.checkBody('email', 'must be valid').isEmail();
       req.checkBody('password', 'must be at least 6 characters long').isLength({min: 6});
       req.checkBody('passwordConfirmation', 'must match password provided').optional().matches(req.body.password);
@@ -13,4 +13,17 @@ module.exports = {
       return next();
     }
   },
+  validatePost(req, res, next){
+    if(req.method === 'POST'){
+      req.checkBody('title', 'must be at least 2 characters').isLength({min: 2});
+      req.checkBody('content', 'must be at least 10 characters long').isLength({min: 10});
+    }
+    const errors = req.validationErrors();
+    if(errors){
+      req.flash('error', errors);
+      return res.redirect(req.headers.referer);
+    } else {
+      return next();
+    }
+  }
 }
